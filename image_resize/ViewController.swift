ccf3698a52d66ca0a1808a56ef238e652861f7af
nakamura-yuta-i7 <yuta@macbook-pro.local>
2016-10-11 21:39:16 +0900

    image resize

--- image_resize/ViewController.swift
+++ image_resize/ViewController.swift
@@ -10,16 +10,78 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func tapSelectImageBtn(_ sender: AnyObject) {
+        let ipc:UIImagePickerController = UIImagePickerController()
+        ipc.delegate = self
+        #if TARGET_IPHONE_SIMULATOR
+            ipc.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        #else
+            // ipc.sourceType = UIImagePickerControllerSourceType.Camera
+        #endif
+        
+        present(ipc, animated:true, completion:nil)
+    }
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var sizeLabel: UILabel!
+    
+    @IBAction func tapResizeImageBtn(_ sender: AnyObject) {
+        print("imageView.image", imageView.image)
+        
+        imageView.image = imageView.image?.resize(size: CGSize(width: 200, height: 200))
+        showImageSize()
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func showImageSize() {
+        let imageData = UIImagePNGRepresentation(imageView.image!)! as NSData
+        sizeLabel.text = String(ByteCountFormatter.string(
+            fromByteCount: Int64(imageData.length),
+            countStyle: ByteCountFormatter.CountStyle.file)
+        )
     }
-
-
 }
 
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if info[UIImagePickerControllerOriginalImage] != nil {
+            let image:UIImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+            self.imageView.image = image
+            debugPrint(image)
+            showImageSize()
+        }
+        //allowsEditingがtrueの場合 UIImagePickerControllerEditedImage
+        //閉じる処理
+        dismiss(animated: true, completion: nil);
+    }
+}
+
+extension UIImage {
+    
+    func resize(size: CGSize) -> UIImage {
+//        let widthRatio = size.width / self.size.width
+//        let heightRatio = size.height / self.size.height
+//        let ratio = (widthRatio < heightRatio) ? widthRatio : heightRatio
+        let resizedSize = size
+        UIGraphicsBeginImageContext(resizedSize)
+        draw(in: CGRect(x: 0, y: 0, width: resizedSize.width, height: resizedSize.height))
+        let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return resizedImage!
+    }
+    
+    //    // 比率だけ指定する場合
+    //    func resize(#ratio: CGFloat) -> UIImage {
+    //        let resizedSize = CGSize(width: Int(self.size.width * ratio), height: Int(self.size.height * ratio))
+    //        UIGraphicsBeginImageContext(resizedSize)
+    //        drawInRect(CGRect(x: 0, y: 0, width: resizedSize.width, height: resizedSize.height))
+    //        let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
+    //        UIGraphicsEndImageContext()
+    //        return resizedImage
+    //    }
+}
